6a94c6bcb10b81060bc7e23f511a58d64f7e80ca
ltnguy <leannanguyen2021@u.northwestern.edu>
2019-01-10 13:40:22 -0600

    in class example

--- lec02/first.py
+++ lec02/first.py
@@ -1,14 +1,22 @@
 # The number of inches in a foot.
 FT_TO_IN = 12
 
+    # DESIGN RECIPE
+    # 1. Header(name,signature,purpose)
+    # 2. Functional examples
+    # 3. Body
+    # 4. Testing
+
+    # Problem: Design a function ft_to_in that
+    # converts a distance in feet to inches
 
 def ft_to_in(ft: float) -> float:
-    """Converts feet to inches.
-
+    """To convert a length from feet to inches.
     >>> ft_to_in(3)
     36
     >>> ft_to_in(10)
     120
     """
+    pass
     return FT_TO_IN * ft
 
